36dee384813b744b8c0a743a844ae52d18a1bcd7
oceminnovatiq <oceminnocatiq@gmail.com>
2026-03-09 22:15:58 +0530

    Fix import paths to match actual file structure

--- nearby-app/backend/api/index.js
+++ nearby-app/backend/api/index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import cors from 'cors';
 
 // Import all handlers
-import { handler as merchantLogin } from '../dist/src/merchant/login.js';
-import { handler as merchantSignup } from '../dist/src/merchant/signup.js';
+import { handler as merchantLogin } from '../dist/src/handlers/auth/merchantLogin.js';
+import { handler as merchantSignup } from '../dist/src/handlers/auth/merchantSignup.js';
 import { handler as merchantUpdateStatus } from '../dist/src/merchant/updateStatus.js';
 import { handler as merchantTrackInteraction } from '../dist/src/merchant/trackInteraction.js';
 
@@ -11,9 +11,9 @@ import { handler as authRegister } from '../dist/src/auth/register.js';
 import { handler as authLogin } from '../dist/src/auth/login.js';
 
 import { handler as getCategories } from '../dist/src/categories/get.js';
-import { handler as getNearbyShops } from '../dist/src/shops/nearby.js';
+import { handler as getNearbyShops } from '../dist/src/shops/getNearby.js';
 
-import { handler as createBroadcast } from '../dist/src/broadcasts/createBroadcastSimple.js';
+import { handler as createBroadcast } from '../dist/src/broadcasts/create.js';
 import { handler as getBroadcasts } from '../dist/src/broadcasts/get.js';
 import { handler as respondToBroadcast } from '../dist/src/broadcasts/respond.js';
 
